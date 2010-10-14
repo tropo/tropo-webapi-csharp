9b492b91016f334594ef7627fc9581a633535d2d
Mark Headd <mheadd@voiceingov.org>
2010-10-15 07:23:43 +0800

    (New) Updated TropoResult and TropoSession classes to use typical C# naming / coding conventions.

--- TropoCSharp/TropoResult.cs
+++ TropoCSharp/TropoResult.cs
@@ -7,91 +7,55 @@ namespace TropoCSharp.Tropo
     /// </summary>
     public class Result
     {
-        private string _sessionId;
-        private string _state;
-        private int _sessionDuration;
-        private int _sequence;
-        private bool _complete;
-        private string _error;
-        private JArray _actions;
-
         /// <summary>
         /// Class constructor.
         /// </summary>
-        /// <param name="JSON">Result JSON submitted from Tropo platform.</param>
-        public Result(string JSON)
+        /// <param name="json">Result JSON submitted from Tropo platform.</param>
+        public Result(string json)
         {
-            JObject results = JObject.Parse(JSON);
-            this._sessionId = (string)results["result"]["sessionId"];
-            this._state = (string)results["result"]["state"];
-            this._sessionDuration = (int)results["result"]["sessionDuration"];
-            this._sequence = (int)results["result"]["sequence"];
-            this._complete = (bool)results["result"]["complete"];
-            this._error = (string)results["result"]["error"];
-            this._actions = (JArray)results["result"]["actions"];
+            JObject results = JObject.Parse(json);
+            SessionId = (string)results["result"]["sessionId"];
+            State = (string)results["result"]["state"];
+            SessionDuration = (int)results["result"]["sessionDuration"];
+            Sequence = (int)results["result"]["sequence"];
+            Complete = (bool)results["result"]["complete"];
+            Error = (string)results["result"]["error"];
+            Actions = (JArray)results["result"]["actions"];
         }
 
         /// <summary>
         /// The state of the session at the time the result was generated.
         /// </summary>
-        public string State
-        {
-            get { return _state; }
-            set { _state = value; }
-        }
+        public string State { get; set; }
 
         /// <summary>
         /// The unique identifier that is available with each session and result payload.
         /// </summary>
-        public string SessionId
-        {
-            get { return _sessionId; }
-            set { _sessionId = value; }
-        }
+        public string SessionId { get; set; }
 
         /// <summary>
         /// The total length of time, in seconds, the current session has been running.
         /// </summary>
-        public int SessionDuration
-        {
-            get { return _sessionDuration; }
-            set { _sessionDuration = value; }
-        }
+        public int SessionDuration { get; set; }
 
         /// <summary>
         /// Represents the number of Tropo payloads returned from your application.
         /// </summary>
-        public int Sequence
-        {
-            get { return _sequence; }
-            set { _sequence = value; }
-        }
+        public int Sequence { get; set; }
 
         /// <summary>
         /// Indicates whether a request resulted in all required fields being completed.
         /// </summary>
-        public bool Complete
-        {
-            get { return _complete; }
-            set { _complete = value; }
-        }
+        public bool Complete { get; set; }
 
         /// <summary>
         /// If the state of the result is an error, refer to this field for the error message.
         /// </summary>
-        public string Error
-        {
-            get { return _error; }
-            set { _error = value; }
-        }
+        public string Error { get; set; }
 
         /// <summary>
         /// The result of the actions requested in the previous payload.
         /// </summary>
-        public JArray Actions
-        {
-            get { return _actions; }
-            set { _actions = value; }
-        }
+        public JArray Actions { get; set; }
     }
 }
