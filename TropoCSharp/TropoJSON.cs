e1f7c7acbaafb67c7438694c11d49670d07fd6d4
Mark Headd <mheadd@voiceingov.org>
2010-10-14 18:59:07 -0400

    (Fix) Added missing TropoJSON.cs file.

---
+++ TropoCSharp/TropoJSON.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using Newtonsoft.Json;
+
+namespace TropoCSharp.Tropo
+{
+    /// <summary>
+    /// A utility class to render a Tropo object as JSON.
+    /// </summary>
+    public static class TropoJSONExtensions
+    {
+        public static string RenderJSON(this Tropo tropo)
+        {
+            tropo.Language = null;
+            tropo.Voice = null;
+            JsonSerializerSettings settings = new JsonSerializerSettings { DefaultValueHandling = DefaultValueHandling.Ignore };
+            return JsonConvert.SerializeObject(tropo, Formatting.None, settings).Replace("\\", "").Replace("\"{", "{").Replace("}\"", "}");
+        }
+    }
+
+}
