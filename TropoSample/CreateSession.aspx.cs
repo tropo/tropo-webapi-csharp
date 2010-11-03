3b7576a96255fb7177d767a929b020a8ad61e6af
unknown <mheadd@.tele-works.local>
2010-11-04 03:29:49 +0800

    (Fix) Missing JsonProperty name for "say" on Message object. (Fix) Adjusted unit tests for Message. (Fix) Modified Ask object to set voice property on self and not Say. (New) Added CreateSession extension method for Tropo Object. (New) Added CreateSession example.

---
+++ TropoSample/CreateSession.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Xml;
+using TropoCSharp.Structs;
+using TropoCSharp.Tropo;
+
+namespace TropoSample
+{
+    public partial class CreateSession : System.Web.UI.Page
+    {
+        // The token for initiating an outbound message from Tropo.
+        // Note - make sure the type of token you are usisng matches the channel/network arguments passed to CreateSession().
+        private string token = "enter-your-token-here";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IDictionary<string, string> parameters = new Dictionary<String, String>();
+            // Enter a phone number here.
+            parameters.Add("connectTo", "1112223333"); 
+            
+            // Select the channel you want to use via the Channel struct.
+            parameters.Add("channel", Channel.Voice);
+
+            // Select the network you want to use with the Network struct.
+            parameters.Add("network", Network.Pstn); 
+
+            // Message is sent as a query string parameter, make sure it is properly encoded.
+            parameters.Add("message", HttpUtility.UrlEncode("This is a test message from C sharp."));
+
+            // Instanntiate a new instance of the Tropo object.
+            Tropo tropo = new Tropo();
+
+            // Create an XML doc to hold the response from the Tropo Session API.
+            XmlDocument doc = new XmlDocument();
+
+            // Load the XML document with the return vallue of the CreateSession() method call.
+            doc.Load(tropo.CreateSession(token, parameters));
+
+            // Assign the relevent return values to 
+            SessionResult.Text = doc.SelectSingleNode("session/success").InnerText.ToUpper();
+            TokenID.Text = doc.SelectSingleNode("session/token").InnerText;
+        }
+    }
+}
