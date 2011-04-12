2d6fac24b2825b99453a3b226a65c34840ef0033
unknown <mheadd@.tele-works.local>
2011-04-12 15:34:36 -0400

    (Add) Modified outbound SMS example.

--- TropoOutboundSMS/Program.cs
+++ TropoOutboundSMS/Program.cs
@@ -12,17 +12,17 @@ namespace OutboundTest
         static void Main(string[] args)
         {
             // The voice and messaging tokens provisioned when your Tropo application is set up.
-            string voiceToken = "00401f421ee41645ac281ba49b6eebcba6f5dabb1d5e022ee71041b1517ae8c562d612a89f2658d5ab72f670";
-            string messagingToken = "004046ba101ab14ea91826d1ec0034195c27974f23397c1aca7bb463f5b77aca2fc56ac136a1ed1d08ca04bc";
+            string voiceToken = "your-voice-token-here";
+            string messagingToken = "your-messaging-token-here";
 
             // A collection to hold the parameters we want to send to the Tropo Session API.
             IDictionary<string, string> parameters = new Dictionary<String, String>();
 
             // Enter a phone number to send a call or SMS message to here.
-            parameters.Add("sendToNumber", "13025888258");
+            parameters.Add("sendToNumber", "15551112222");
 
             // Enter a phone number to use as the caller ID.
-            parameters.Add("sendFromNumber", "15853260554");
+            parameters.Add("sendFromNumber", "15551113333");
 
             // Select the channel you want to use via the Channel struct.
             string channel = Channel.Text;
