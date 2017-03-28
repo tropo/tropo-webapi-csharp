51d47a6f239b46cf212bab5bf78628bc474a9993
Frank Yu <xiangjun919@gmail.com>
2017-03-28 01:42:12 -0700

    frank test SUP-2939

--- TropoSample/AskTest.aspx.cs
+++ TropoSample/AskTest.aspx.cs
@@ -15,7 +15,7 @@ namespace TropoSample
             // Create a new instance of the Tropo object.
             Tropo tropo = new Tropo();
 
-            tropo.Voice = Voice.UsEnglishFemale_Susan;
+            //tropo.Voice = Voice.UsEnglishFemale_Susan;
             
             // Create an array of signals - used to interupt the Ask.
             string[] signals = new string[] {"endCall", "tooLong"};
@@ -23,12 +23,23 @@ namespace TropoSample
             // A prompt to use with the Ask.
             Say say = new Say("This is an Ask test with events. Please enter 1, 2 or 3.");
 
+            // A prompt to use with the Ask.
+            Say franksay = new Say("This is frank Ask test with events. Please enter 4, 6 or 2.");
+
+            // A prompt to use with the Ask.On.
+            Say sayon = new Say("This is an Ask on say");
+
             // Choices for the Ask.
             Choices choices = new Choices("1,2,3");
 
+            // frank's Choices for the Ask.
+            Choices frankchoices = new Choices("4,6,2");
+
             // Set up the dialog.
             tropo.Ask(5, signals, false, null, choices, null, "test", Recognizer.UsEnglish, true, say, 30);
-            tropo.Hangup();
+            tropo.Ask(5, signals, false, null, frankchoices, null, "franktest", Recognizer.UsEnglish, true, franksay, 30);
+            tropo.On("continue", "TropoResult.aspx", sayon);
+            //tropo.Hangup();
 
             // Render the dialog JSON for Tropo to consume.
             Response.Write(tropo.RenderJSON());
