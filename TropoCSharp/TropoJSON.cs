b4103e0313cbeae43fdba98039ab7886ef8e897c
Frank Yu <xiangjun919@gmail.com>
2017-05-09 21:46:10 -0700

    neat the code

--- TropoCSharp/TropoJSON.cs
+++ TropoCSharp/TropoJSON.cs
@@ -23,25 +23,9 @@ namespace TropoCSharp.Tropo
             tropo.Language = null;
             tropo.Voice = null;
             JsonSerializerSettings settings = new JsonSerializerSettings { DefaultValueHandling = DefaultValueHandling.Ignore };
-            //return JsonConvert.SerializeObject(tropo, Formatting.None, settings).Replace("\\", "").Replace("\"{", "{").Replace("}\"", "}");
-            Version v = GetVersion();
-            // {Major}.{Minor}.{Build}.{Revision}
-            int Major = v.Major;
-            int Minor = v.Minor;
-            int Build = v.Build;
-            int Revision = v.Revision;
-            //response.AddHeader("WebAPI-Lang-Ver", "webapi_CSharp" + v.ToString());
             response.AddHeader("WebAPI-Lang-Ver", "CSharp V15.7.0 2017-05-12");
             response.Write(JsonConvert.SerializeObject(tropo, Formatting.None, settings).Replace("\\", "").Replace("\"{", "{").Replace("}\"", "}"));
 
-        }
-
-        public static Version GetVersion()
-        {
-            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            System.Reflection.AssemblyName assemblyName = assembly.GetName();
-            Version version = assemblyName.Version;
-            return version;
         }
 
     }
