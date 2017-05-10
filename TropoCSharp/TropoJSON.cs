e9ada166eb0198ba73583ebe9d51c295920a6945
Frank Yu <xiangjun919@gmail.com>
2017-05-10 03:01:18 -0700

    SNAPSHOT version

--- TropoCSharp/TropoJSON.cs
+++ TropoCSharp/TropoJSON.cs
@@ -23,9 +23,19 @@ namespace TropoCSharp.Tropo
             tropo.Language = null;
             tropo.Voice = null;
             JsonSerializerSettings settings = new JsonSerializerSettings { DefaultValueHandling = DefaultValueHandling.Ignore };
-            response.AddHeader("WebAPI-Lang-Ver", "CSharp V15.7.0 2017-05-12");
+            Version v = GetVersion();
+            response.AddHeader("WebAPI-Lang-Ver", "CSharp V15.7.1 " +v.Build.ToString() +"." + v.Revision.ToString()  + "-SNAPSHOT");
+            //response.AddHeader("WebAPI-Lang-Ver", "CSharp V15.7.1 SNAPSHOT");
             response.Write(JsonConvert.SerializeObject(tropo, Formatting.None, settings).Replace("\\", "").Replace("\"{", "{").Replace("}\"", "}"));
 
+        }
+
+        public static Version GetVersion()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            System.Reflection.AssemblyName assemblyName = assembly.GetName();
+            Version version = assemblyName.Version;
+            return version;
         }
 
     }
