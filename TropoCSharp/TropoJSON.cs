339db2264ff5db21be1d4b71d65867caa54d374c
Frank Yu <xiangjun919@gmail.com>
2017-05-15 02:03:08 -0700

    Add WebAPI Header 15.8.0 SNAPSHOT

--- TropoCSharp/TropoJSON.cs
+++ TropoCSharp/TropoJSON.cs
@@ -14,21 +14,10 @@ namespace TropoCSharp.Tropo
             tropo.Language = null;
             tropo.Voice = null;
             JsonSerializerSettings settings = new JsonSerializerSettings { DefaultValueHandling = DefaultValueHandling.Ignore };
-            Version v = GetVersion();
-            //response.AddHeader("WebAPI-Lang-Ver", "CSharp V15.7.1 " +v.Build.ToString() +"." + v.Revision.ToString()  + "-SNAPSHOT");
             response.AddHeader("WebAPI-Lang-Ver", "CSharp V15.8.0 SNAPSHOT");
             response.Write(JsonConvert.SerializeObject(tropo, Formatting.None, settings).Replace("\\", "").Replace("\"{", "{").Replace("}\"", "}"));
 
         }
-
-        public static Version GetVersion()
-        {
-            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            System.Reflection.AssemblyName assemblyName = assembly.GetName();
-            Version version = assemblyName.Version;
-            return version;
-        }
-
     }
 
 }
