3472e9c9b02a5594489464de521f1a6826b76e15
Frank Yu <xiangjun919@gmail.com>
2017-06-15 01:17:51 -0700

    SUP-3160 A customer would like to see an Inbound SMS example available in the Tropo C# repository

---
+++ TropoSample/SUP3160startCall.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Web.UI;
+using Newtonsoft.Json;
+using TropoCSharp.Tropo;
+
+namespace TropoSamples
+{
+    /// <summary>
+    /// An example script that receives a Tropo Session create POST and returns JSON for sending an outbound message.
+    /// Note - use in conjnction withe the TropoOutboundSMS example application.
+    /// For further information, see http://blog.tropo.com/2011/04/14/sending-outbound-sms-with-c/
+    /// </summary>
+    public partial class SUP3160startCall : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (StreamReader reader = new StreamReader(Request.InputStream))
+            {
+                // Get the JSON submitted from Tropo.
+                string sessionJSON = TropoUtilities.parseJSON(reader);
+
+                // Create a new instance of the Tropo object.
+                Tropo tropo = new Tropo();
+
+                try
+                {
+                    // Create a new Session object and pass in the JSON submitted from Tropo.
+                    Session tropoSession = new Session(sessionJSON);
+
+                    // Get parameters submitted with Session API call.
+                    string numberToDial = tropoSession.Parameters.Get("numberToDial");
+                    string textMessageBody = tropoSession.Parameters.Get("textMessageBody");
+
+                    string sendFromNumber = "+17754641173";
+                    string channel = "TEXT";
+                    string network = "SMS";
+
+                    // Send an outbound message.
+                    //tropo.Call(numberToDial, sendFromNumber, network, channel, true, 60, null);
+                    tropo.Call(numberToDial);
+                    tropo.Say("You will hear current time " + textMessageBody);
+                    tropo.Hangup();
+                }
+
+                catch (JsonReaderException ex)
+                {
+                    EventLog log = new EventLog();
+                    log.Source = "TROPOWEBAPI";
+                    log.WriteEntry("Tropo WebAPI Exception " + ex.Message, EventLogEntryType.Error);
+                    Response.StatusCode = 500;
+                    tropo.Say("An error occured in the application. Bad JSON");
+
+                }
+
+                catch (Exception ex)
+                {
+                    EventLog log = new EventLog();
+                    log.Source = "TROPOWEBAPI";
+                    log.WriteEntry("Tropo WebAPI Exception " + ex.Message, EventLogEntryType.Error);
+                    Response.StatusCode = 500;
+                    tropo.Say("An error occured in the application.");
+                }
+
+                finally
+                {
+                    tropo.RenderJSON(Response);
+                }
+            }
+        }
+    }
+}
---
+++ TropoSample/TropoInboundSMS.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using TropoCSharp.Tropo;
+using System.Web.UI;
+using TropoCSharp.Structs;
+using System.Web;
+using System.Xml;
+using System.Collections.Generic;
+
+namespace TropoSamples
+{
+    /// <summary>
+    /// A simple example showing how to access properties of the Session object.
+    /// </summary>
+    public partial class TropoInboundSMS : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (StreamReader reader = new StreamReader(Request.InputStream))
+            {
+                // Get the JSON submitted from Tropo.
+                string sessionJSON = TropoUtilities.parseJSON(reader);
+
+                // Create a new instance of the Tropo class.
+                Tropo tropo = new Tropo();
+
+                // Create a new Session object and pass in the JSON submitted from Tropo.
+                Session tropoSession = new Session(sessionJSON);
+
+                // Create an XML doc to hold the response from the Tropo Session API.
+                XmlDocument doc = new XmlDocument();
+                string token = "584c466166734645667651785355666c7652564c495756676c4c664e786976415655584e6741525155536b6c";
+                // A collection to hold the parameters we want to send to the Tropo Session API.
+                IDictionary<string, string> parameters = new Dictionary<String, String>();
+
+                // Enter a phone number to send a call or SMS message to here.
+                parameters.Add("numberToDial", "+8613466549249");
+                parameters.Add("textMessageBody", tropoSession.InitialText);
+
+                doc.Load(tropo.CreateSession(token, parameters));
+            }
+        }
+    }
+}
