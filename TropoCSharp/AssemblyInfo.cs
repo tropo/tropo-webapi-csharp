36e0475b97c47ced603d9dfdd3a0ba4d47d14ef1
Frank Yu <xiangjun919@gmail.com>
2018-05-17 01:11:25 -0700

    release 15.11.0

--- TropoCSharp/AssemblyInfo.cs
+++ TropoCSharp/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("TropoCSharp.TropoClasses")]
 [assembly: AssemblyDescription("C# Class Library for the Tropo WabAPI")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Cisco")]
+[assembly: AssemblyConfiguration("debug")]
+[assembly: AssemblyCompany("Cisco Systems, Inc.")]
 [assembly: AssemblyProduct("Tropo WebAPI")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("15.8.*")]
+[assembly: AssemblyVersion("15.11.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
